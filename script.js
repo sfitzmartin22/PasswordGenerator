2360b7f4d94fa581923870b46641279f311ca762
Sean Fitzmartin <sfitzmartin22@gmail.com>
2021-02-11 21:11:43 -0500

    added if statements for password length requirements, commented out the javascript lines for ease of use. updated symbols to include space

--- script.js
+++ script.js
@@ -7,50 +7,59 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// object that has all the variables and potential variable assignments in it to for the generate password to pass through //
+// object that has all the potential variable assignments in it for the generate password to pass through //
 var characterpool = {
   lowercase: "abcdefghijklmnopqrstuvwxyz",
   uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   number: "0123456789",
-  symbol: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+  symbol: " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~", // added \ in front of "problem characters that would end string"  //
 };
 
-// function that defines password specifications and then assigns //
+// function that actually creates the password //
 function generatePassword() {
   var password = "";
   var passwordCharSet = "";
 
-  var length = window.prompt("Enter a number from 8 to 128 for your password length.");
+  // user input for how many characters they want their password to be //
+ var length = window.prompt("Enter a number from 8 to 128 for your password length.");
 
+ // Message to user if they do not pick a number in the range //
+ if (length < 8 || length > 128) {
+   alert("You must select a number between 1 and 128 to continue")
+ }
+
+ // if the user selects the length of the password in the appropriate range then it will ask them what character types they want to use //
+ if (length >= 8 && length <= 128){
   var lowercase = window.confirm("Would you like to use lowercase letters?");
   if (lowercase === true) {
-    passwordCharSet += characterpool.lowercase;
+    passwordCharSet += characterpool.lowercase; // when user selects to use lowercase, this line adds the corresponding character pool to the password character set //
   };
 
   var uppercase = window.confirm("Would you like to use uppercase letters?");
   if (uppercase === true) {
-    passwordCharSet += characterpool.uppercase;
+    passwordCharSet += characterpool.uppercase; // when user selects to use uppercase, this line adds the corresponding character pool to the password character set //
   };
 
   var symbols = window.confirm("Would you like to use symbols?");
   if (symbols === true) {
-    passwordCharSet += characterpool.symbol;
+    passwordCharSet += characterpool.symbol; // when user selects to use symbols, this line adds the corresponding character pool to the password character set //
   };
 
   var numbers = window.confirm("Would you like to use numbers?");
   if (numbers === true) {
-    passwordCharSet += characterpool.number;
+    passwordCharSet += characterpool.number; // when user selects to use numbers, this line adds the corresponding character pool to the password character set //
   };
 
-  for (let i = 0; i < length; i++) {
+// for statement that generates a random character from the password character set and adds it to the password.  This is done however many times is necessary to satisfy the users selected length //  
+  for (let i = 0; i < length; i++) { 
     password += passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)]
   }
   return password;
 }
 
+}
